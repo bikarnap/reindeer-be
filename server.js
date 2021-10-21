52bd7e443823036bad388ea9f877e3d39788d976
Bikarna Pokharel <bikarna.pokharel@tuni.fi>
2021-10-21 14:31:08 +0545

    Add dummy food data

--- server.js
+++ server.js
@@ -3,12 +3,97 @@ const app = express();
 
 const PORT = 8000;
 
+const foodData = [
+    {
+      id: "1",
+      item: "item01",
+      ingredient: "potatos,cocumber",
+      available: true,
+      country: "Finland",
+      discount: "FI",
+      image: "Pic1",
+      foodChoices: ["Non-Veg"],
+    },
+    {
+      id: "2",
+      item: "item02",
+      ingredient: "garlic,Lamb,salad",
+  
+      available: true,
+      country: "Finland",
+      discount: "FI",
+      image: "Pic2",
+  
+      foodChoices: ["Non-Veg"],
+    },
+    {
+      id: "3",
+      item: "item03",
+      ingredient: "garlic, salad, Chilli ",
+      available: true,
+      country: "Finland",
+      discount: "FI",
+      image: "Pic3",
+      foodChoices: ["Veg"],
+    },
+    {
+      id: "4",
+      item: "item04",
+      ingredient: "garlic, salad, Chicken, Chilli",
+      available: true,
+      country: "Finland",
+      discount: "FI",
+      image: "Pic4",
+      foodChoices: ["Lacto Veg"],
+    },
+    {
+      id: "5",
+      item: "item05",
+      ingredient: "garlic, salad, Chicken,Chilli",
+      available: true,
+      country: "Finland",
+      discount: "FI",
+      image: "Pic5",
+      foodChoices: ["Non-Veg"],
+    },
+    {
+      id: "6",
+      item: "item06",
+      ingredient: "garlic, salad, Chicken, Chilli",
+      available: true,
+      country: "Finland",
+      discount: "FI",
+      image: "Pic6",
+      foodChoices: ["Lactose Free"],
+    },
+    {
+      id: "7",
+      item: "item07",
+      ingredient: "garlic, salad, Chicken, Chilli",
+      available: true,
+      country: "Finland",
+      discount: "FI",
+      image: "",
+      foodChoices: ["Gluton free"],
+    },
+    {
+      id: "8",
+      item: "item08",
+      ingredient: "garlic, salad, Chicken, Chilli",
+      available: true,
+      country: "Finland",
+      discount: "FI",
+      image: "",
+      foodChoices: ["Gluton free"],
+    }
+]
+
 const getWelcomeMessage = (req, res) => {
     res.send('<em>Reindeer food service for train passengers</em>');
 };
 
-const getAllMenu = (req, res) => {
-    res.send('<h2>Available food menu</h2>')
+const getAllMenus = (req, res) => {
+    res.json(foodData)
 };
 
 const postDepartureInfo = (req, res) => {
@@ -19,7 +104,7 @@ const postDepartureInfo = (req, res) => {
 // GET
 app.get('/', getWelcomeMessage);
 
-app.get('/menu', getAllMenu);
+app.get('/api/menus', getAllMenus);
 
 // POST
 app.post('/departure', postDepartureInfo);
