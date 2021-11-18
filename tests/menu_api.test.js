f3e9a77391bdda15288124028f0818ace5528d24
Bikarna Pokharel <bikarna.pokharel@tuni.fi>
2021-11-18 14:46:37 +0545

    Update

--- tests/menu_api.test.js
+++ tests/menu_api.test.js
@@ -19,3 +19,25 @@ test('the first menu item is item01', async () => {
   const response = await api.get('/api/menus');
   expect(response.body[0].item).toBe('item01');
 });
+
+test('a valid food menu can be added', async () => {
+  const menuItem = {
+    id: '9',
+    item: 'item09',
+    ingredient: 'garlic, salad, Chicken, Chilli',
+    available: true,
+    country: 'Finland',
+    discount: 'FI',
+    image: './images/2.jpg',
+    foodChoices: ['Gluton free'],
+  };
+
+  await api
+    .post('/api/menus')
+    .send(menuItem)
+    .expect(200)
+    .expect('Content-type', /application\/json/);
+
+  const response = await api.get('/api/menus');
+  expect(response.body).toHaveLength(9);
+});
